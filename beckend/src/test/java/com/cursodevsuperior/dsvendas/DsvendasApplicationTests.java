58d159f9d4c8b102986e937a22271355d0db7ff3
Fernando Ramos <fernandoramosrz@yahoo.com.br>
2021-11-01 22:51:55 -0300

    projetos criados

---
+++ beckend/src/test/java/com/cursodevsuperior/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursodevsuperior.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
